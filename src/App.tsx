e561a7b8e82dbe2574ba4fc048043ac159aa55f8
mahadihasandevx <mahadihasandevx@gmail.com>
2026-09-13 20:13:48 +0600

    Assinment Done

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Banner from "./Component/Banner";
 
 
 const playersFetch = async():Promise<Iplayer[]> =>{
-    const res = await fetch('/data.json');
+    const res = await fetch('./data.json');
     const data = await res.json();
 
      return data ;
